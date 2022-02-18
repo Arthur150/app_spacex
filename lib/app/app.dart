33900c5b39eb2fd78b01d85b0e08c368702b58eb
Arthur <arthur.chevauchet@gmail.com>
2022-02-18 17:29:59 +0100

    app file fix

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -18,7 +18,7 @@ class App extends StatelessWidget {
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
+        // application has a red toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
